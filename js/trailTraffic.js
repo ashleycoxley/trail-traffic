2a3bf5d0523c6b3c363d303733baa8820c8c694e
ashleycoxley <ashleycoxley@gmail.com>
2016-11-25 07:59:59 -0800

    Basic map

---
+++ js/trailTraffic.js
@@ -0,0 +1,7 @@
+var map;
+	function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 37.810690, lng: -122.170217},
+        zoom: 14
+      });
+    }
