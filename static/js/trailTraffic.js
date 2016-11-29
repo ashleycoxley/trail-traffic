18df2112ce7448bd9ee2ebcf9f15e36d8d05d6d3
ashleycoxley <ashleycoxley@gmail.com>
2016-11-29 11:33:57 -0800

    Added Knockout list of segments
    Added list of segments to map page, generated using Knockout, and bound each list item to a Google Maps segment. At the moment, clicking on a segment in the list changes the color of the selected segment.

--- static/js/trailTraffic.js
+++ static/js/trailTraffic.js
@@ -1,7 +1,7 @@
 function initializeMap() {
   // Google map initialization, to be called in $(document).ready()
   map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 37.810690, lng: -122.170217},
+      center: {lat:37.813536, lng:-122.178588},
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
@@ -22,35 +22,52 @@ $(document).ready(function() {
   var ViewModel = function() {
     var self = this;
     var map = initializeMap();
+    var segmentLines = [];
+    this.segmentList = ko.observableArray([]);
 
     $.get('/segments/redwood', function(data) {
       var redwoodSegments = data.segments;
-      redwoodSegments.forEach(function(segment) {
+      redwoodSegments.forEach(function(segment, idx) {
         var segmentID = segment.id;
+        var segmentName = segment.name;
+        // Populate KO array
+        self.segmentList.push(new Segment(segment));
+
         // Pre-load traffic data into infoWindow
         infoWindow = new google.maps.InfoWindow();
         infoWindow.setOptions({
-          content: setTrafficData(segment.id, infoWindow)
+          content: setTrafficData(segment.id, infoWindow, segmentName)
         });
 
         // Draw segment line
         polyLine = segment.map.polyline;
         var decodedSegment = google.maps.geometry.encoding.decodePath(polyLine);
 
-        segmentLine = new google.maps.Polyline({
+        segmentLines[idx] = new google.maps.Polyline({
           path: decodedSegment,
           geodesic: true,
           strokeColor: '#FF0000',
           strokeOpacity: 1,
           strokeWeight: 3
         });
-        segmentLine.setMap(map);
+        segmentLines[idx].setMap(map);
 
         // Add click handler to segment that displays infoWindow with traffic
-        setSegmentClickHandler(segmentLine, infoWindow, segmentID);
+        setSegmentClickHandler(segmentLines[idx], infoWindow, segmentID);
 
       });
     });
+    
+    this.selectSegment = function(idx) {
+      segmentLines.forEach(function(segment) {
+        segment.setOptions({
+          strokeColor: "#FF0000"
+        });
+      });
+      segmentLines[idx].setOptions({
+        strokeColor: "#000000"
+      });
+    };
 
     function setSegmentClickHandler(segmentLine, infoWindow, segmentID) {
       var currentSegment = segmentLine;
@@ -62,10 +79,11 @@ $(document).ready(function() {
       });
     }
 
-    function setTrafficData(segmentID, infoWindow) {
+    function setTrafficData(segmentID, infoWindow, segmentName) {
       var trafficURL = '/traffic/' + segmentID;
       $.get(trafficURL, function(data) {
-        var traffic_content = data.traffic_count.toString() +
+        var traffic_content = "<b>" + segmentName + "</b>" +
+                              data.traffic_count.toString() +
                               " bicyclists in the past 7 days";
         infoWindow.setOptions({
           content: traffic_content
