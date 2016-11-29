2155314abf99bcea8827f93f8134194b8c798acb
ashleycoxley <ashleycoxley@gmail.com>
2016-11-29 15:18:12 -0800

    Added list and segment search filter
    Added search box that filters the visible list of trail segments.
    
    In order to enable filtering of map lines based on the segment search feature, code was refactored such that Google map elements are now part of the Segment data model (rather than the controller/ViewModel). By default, map segment lines are set to show, and when search filters are applied, the filtered segments are set to hide.

--- static/js/trailTraffic.js
+++ static/js/trailTraffic.js
@@ -13,84 +13,99 @@ function initializeMap() {
 $(document).ready(function() {
 
   var Segment = function(data) {
-    this.startPt = ko.observable(data.start_latlng);
-    this.endPt = ko.observable(data.end_latlng);
-    this.polyLine = ko.observable(data.map.polyline);
+    var own = this;
+    this.id = ko.observable(data.id);
     this.name = ko.observable(data.name);
+
+    this.infoWindow = new google.maps.InfoWindow({
+      content: "<b>" + data.name + "</b><br>"
+    });
+
+    this.setTrafficData = ko.computed(function() {
+      var trafficURL = '/traffic/' + own.id();
+      $.get(trafficURL, function(data) {
+        var traffic_content = "<b>" + own.name() + "</b><br>" +
+                              data.traffic_count.toString() +
+                              " bicyclists in the past 7 days";
+        own.infoWindow.setOptions({
+          content: traffic_content
+        });
+      });
+    });
+
+    this.decodedSegment = ko.computed(function() {
+      return google.maps.geometry.encoding.decodePath(data.map.polyline);
+    });
+
+    this.mapLine = ko.computed(function() {
+      return new google.maps.Polyline({
+        path: own.decodedSegment(),
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1,
+        strokeWeight: 3
+      });
+    });
+
+    this.displayedOnMap = ko.observable(true);
+
+    this.displayLine = ko.computed(function() {
+      if (own.displayedOnMap() === true) {
+        own.mapLine().setMap(map);
+      } else {
+        own.mapLine().setMap(null);
+      }
+    });
+
+    this.setSegmentClickHandler = ko.computed(function() {
+      own.mapLine().addListener('click', function(event) {
+        var currentEvent = event;
+        own.infoWindow.setPosition(currentEvent.latLng);
+        own.infoWindow.open(map);
+      });
+    });
   };
+  
 
   var ViewModel = function() {
     var self = this;
     var map = initializeMap();
     var segmentLines = [];
     this.segmentList = ko.observableArray([]);
+    this.searchInput = ko.observable();
 
     $.get('/segments/redwood', function(data) {
       var redwoodSegments = data.segments;
       redwoodSegments.forEach(function(segment, idx) {
-        var segmentID = segment.id;
-        var segmentName = segment.name;
-        // Populate KO array
         self.segmentList.push(new Segment(segment));
-
-        // Pre-load traffic data into infoWindow
-        infoWindow = new google.maps.InfoWindow();
-        infoWindow.setOptions({
-          content: setTrafficData(segment.id, infoWindow, segmentName)
-        });
-
-        // Draw segment line
-        polyLine = segment.map.polyline;
-        var decodedSegment = google.maps.geometry.encoding.decodePath(polyLine);
-
-        segmentLines[idx] = new google.maps.Polyline({
-          path: decodedSegment,
-          geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 1,
-          strokeWeight: 3
-        });
-        segmentLines[idx].setMap(map);
-
-        // Add click handler to segment that displays infoWindow with traffic
-        setSegmentClickHandler(segmentLines[idx], infoWindow, segmentID);
-
       });
     });
-    
-    this.selectSegment = function(idx) {
-      segmentLines.forEach(function(segment) {
-        segment.setOptions({
+
+    this.selectSegment = function(displayListItem) {
+      self.segmentList().forEach(function(segment) {
+        segment.mapLine().setOptions({
           strokeColor: "#FF0000"
         });
       });
-      segmentLines[idx].setOptions({
+      displayListItem.mapLine().setOptions({
         strokeColor: "#000000"
       });
     };
 
-    function setSegmentClickHandler(segmentLine, infoWindow, segmentID) {
-      var currentSegment = segmentLine;
-      var currentInfoWindow = infoWindow;
-      currentSegment.addListener('click', function(event) {
-        var currentEvent = event;
-        infoWindow.setPosition(currentEvent.latLng);
-        infoWindow.open(map);
-      });
-    }
-
-    function setTrafficData(segmentID, infoWindow, segmentName) {
-      var trafficURL = '/traffic/' + segmentID;
-      $.get(trafficURL, function(data) {
-        var traffic_content = "<b>" + segmentName + "</b>" +
-                              data.traffic_count.toString() +
-                              " bicyclists in the past 7 days";
-        infoWindow.setOptions({
-          content: traffic_content
+    this.filterSegments = ko.computed(function() {
+      if (!self.searchInput()) {
+        self.segmentList().forEach(function(segment) {
+          segment.displayedOnMap(true);
         });
+        return self.segmentList();
+      }
+      var searchInputLower = self.searchInput().toLowerCase();
+      return ko.utils.arrayFilter(self.segmentList(), function(segment) {
+        var match = segment.name().toLowerCase().indexOf(searchInputLower) >= 0;
+        segment.displayedOnMap(match);
+        return match;
       });
-    }
-
+    });
   };
 
   ko.applyBindings(new ViewModel());
