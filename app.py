f50f03e93d9b767cd0b6e030de7e69600cfcb3a7
ashleycoxley <ashleycoxley@gmail.com>
2016-12-08 22:27:56 -0800

    Styling and responsive design
    Page-wide styling implemented, along with responsive size changes and a complete UI change for small devices.

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def get_traffic(segment_id, time_parameter):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.config['DEBUG'] = True
-    app.run(host='localhost', port=5000)
+    app.run(host='localhost', port=5000, threaded=True)
